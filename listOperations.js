e76fb312de5fc3e6df0a959e0417c2b373d3e695
RogCampbell <roger.w.campbell@ttu.edu>
2017-02-08 12:21:37 -0600

    Added current list and set operations
    some functions are not complete and won't be until tuples are added

---
+++ listOperations.js
@@ -0,0 +1,19 @@
+/**
+ * Created by theso_000 on 2/6/2017.
+ */
+/*
+
+ len: sequence indexing: If u is a sequence, then |u| is the length of u.
+
+ listsub: If s is a list of length n and 1 ≤ i ≤ n, then we may write s[i] for the i'th member of u.
+ This operation associates together from the left with tuple indexing; e.g., we may write  s[i][j] for (s[i])[j].
+
+ listeq: if s and t are lists of the same type we may write s = t to mean that s and t are the same list
+
+ cat: If s and t are lists of the same type we may write s + t for the concatenation of s and t.
+ For example, <1,2,3> + <4,5,6> = <1,2,3,4,5,6>
+
+ slice: f s is a list and 1 ≤ i, j ≤ |s|, we may write s[i..j] for <si,...,sj>.
+ If i > j, the result is the empty list.
+
+ */
---
+++ sets.js
@@ -0,0 +1,67 @@
+/**
+ * Created by theso_000 on 1/30/2017.
+ */
+function testOne() {
+    window.alert(JSON.stringify(toSet([1,1,"apple",2,2,"apple", [1,2,3]])));
+}
+// takes an array and converts it to a sorted set with no duplicates
+function toSet(arr){
+    //sorts the array, then filters out repeat occurrences by position
+    return arr.sort().filter(function(item, pos, self) {
+        return self.indexOf(item) == pos;
+    });
+}
+
+// returns true if element ∈ setA, false otherwise
+function inSet(element, setA){
+    return setA.indexOf(element) != -1;
+}
+
+// returns true if setA = setB false otherwise
+function seteq(setA, setB) {
+    if(setA.length != setB.length){
+        return false
+    }else{
+        for(i=0; i<setA.length; i++){
+            if(setA[i] !== setB[i]){
+                return false
+            }
+        }
+        return true;
+    }
+}
+
+// returns setA ∪ setB
+function union(setA, setB) {
+    //concat A and B then remove duplicates
+    return toSet(setA.concat(setB));
+}
+
+// returns setA ∩ setB
+function intersect(setA, setB){
+    return setA.concat(setB).sort().filter(function(item, pos, self) {
+        return self.indexOf(item) != pos;
+    });
+}
+
+// returns setA / setB
+function setDiff(setA, setB){
+    //removes from A all items in setA ∩ setB
+    return setA.filter(function (item, pos, self){
+        return !inSet(item,intersect(setA,setB));
+    });
+}
+
+// returns setA x setB
+function cross(setA, setB){
+
+}
+
+// returns cardinality of setA
+function card(setA) {
+    setA.length;
+}
+
+function pow(setA){
+
+}
